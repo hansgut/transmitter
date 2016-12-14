a3c94c8a954b68a1e85d428beea260bf4c25b15f
hansgut <hansgut@outlook.com>
2016-12-14 11:12:46 +0300

    Add files via upload

--- Resources/Resource.Designer.cs
+++ Resources/Resource.Designer.cs
@@ -75,17 +75,20 @@ namespace App1
 			// aapt resource value: 0x7f050001
 			public const int connect_button = 2131034113;
 			
+			// aapt resource value: 0x7f050002
+			public const int current_song = 2131034114;
+			
 			// aapt resource value: 0x7f050000
 			public const int input_ip = 2131034112;
 			
-			// aapt resource value: 0x7f050004
-			public const int next_button = 2131034116;
-			
-			// aapt resource value: 0x7f050002
-			public const int prev_button = 2131034114;
+			// aapt resource value: 0x7f050005
+			public const int next_button = 2131034117;
 			
 			// aapt resource value: 0x7f050003
-			public const int start_button = 2131034115;
+			public const int prev_button = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int start_button = 2131034116;
 			
 			static Id()
 			{
